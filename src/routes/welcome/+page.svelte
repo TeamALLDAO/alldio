26628e6c1845a9c434a4b9b89213b191ff6b94d2
dxtobi <akanbijosephtobi@gmail>
2023-08-18 04:23:10 -0700

    fixed

--- src/routes/welcome/+page.svelte
+++ src/routes/welcome/+page.svelte
@@ -1,20 +1,9 @@
 <script>
-	import ThirdSection from "../../components/ThirdSection.svelte";
-	import AboutUs from "../../components/landing/AboutUs.svelte";
-	import SecondSection from "../../components/welcom/SecondSection.svelte";
-	import TopSection from "../../components/welcom/TopSection.svelte";
-
 
 </script>
 <section>
 
-	<TopSection />
-	<div class="my-10" />
-	<SecondSection/>
-	<div class="my-10" />
-	<ThirdSection/>
-	<div class="my-10" />
-	<AboutUs />
-	<div class="my-10" />
+
+	
 
 </section>
